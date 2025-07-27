5c271656aae0d42b907403c6a2c03458d9de4172
san376 <155067913+san376@users.noreply.github.com>
2025-07-28 01:47:40 +0530

    Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import logo1 from '../assets/logo1.jpeg'
+import logo2 from '../assets/logo2.png'
+
 
 const Navbar = () => {
   return (
     <div className='flex items-center justify-between text:sm py-4 mb-4 border-b border-gray-400'>
-      <div>
-
-      </div>
+        <img src={logo2} alt="" className='w-80 h-40 rounded-full' />
       <div className=' md:flex items-start gap-5 font-medium'>
          <NavLink to='/'>
             Home
